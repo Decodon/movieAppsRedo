8876525c9eaa82199259548d2d3aa75abe514e37
Decodon <20095352@mail.wit.ie>
2022-08-21 19:01:20 +0100

    Added Upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { getMovies } from "../api/tmdb-api";
+import { getUpcomingMovies } from "../api/tmdb-api";
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
   const favourites = movies.filter((m) => m.favourite);
   localStorage.setItem("favourites", JSON.stringify(favourites));
 
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
-    getMovies().then((movies) => {
+    getUpcomingMovies().then((movies) => {
       setMovies(movies);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -16,8 +23,9 @@ const UpcomingMoviesPage = (props) => {
 
   return (
     <PageTemplate
-      title="Discover Movies"
+      title="Discover Upcoming Movies"
       movies={movies}
+      selectFavourite={addToFavourites}
     />
   );
 };
